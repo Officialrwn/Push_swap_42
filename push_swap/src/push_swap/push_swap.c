459c33cf74f95375279e753ddbf2a7fb303fbac8
Leo MPB <vanleotran@gmail.com>
2022-06-03 22:57:26 +0300

    refactored code and fixed norm

--- push_swap/src/push_swap/push_swap.c
+++ push_swap/src/push_swap/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: leo <leo@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/11 23:38:59 by leo               #+#    #+#             */
-/*   Updated: 2022/06/01 21:42:34 by leo              ###   ########.fr       */
+/*   Updated: 2022/06/03 22:56:58 by leo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ static void	init_intarrays(t_nums *arr, int size)
 	arr->lis_head = NULL;
 	arr->lis_tail = NULL;
 	arr->lis = (int *)malloc(sizeof(int) * size);
-	arr->num = (int  *)malloc(sizeof(int) * size);
+	arr->num = (int *)malloc(sizeof(int) * size);
 	if (!arr->num || !arr->lis)
 		exit (1);
 	arr->size = size;
@@ -26,7 +26,7 @@ static void	init_intarrays(t_nums *arr, int size)
 		arr->lis[size] = 1;
 }
 
-static int get_closest_non_lis(t_node *lis, int flag)
+static int	get_closest_non_lis(t_node *lis, int flag)
 {
 	t_node	*current;
 	t_node	*temp;
@@ -50,47 +50,27 @@ static int get_closest_non_lis(t_node *lis, int flag)
 	return (res);
 }
 
-static void	rotate_stacks(t_struct *st, t_op op, int flag, int mean)
+static t_nums	*non_lis_to_stackb(t_struct *st, t_nums *arr, int *n, int flag)
 {
-	t_op exec_op;
-
-	exec_op = op;
-	// return ;
-	if (op == RA && !flag && st->stack_b->num < st->tail_b->num) // if RA and head b < tail b then RR
-		exec_op = RR;
-	else if (op == RRA && !flag && st->tail_b->num < st->stack_a->num
-		&& st->tail_b->num > st->stack_b->num) // if tail b < head a and tail b > head b
-		exec_op = RRR;
-	rotate(st, exec_op);
-	mean = 0;
-	flag = 0;
- }
-
-static t_nums *init_stackb(t_struct *st, t_nums *arr, int *n, int flag)
-{
-	t_op	op;
-	int count = *n;
-
-	op = RA; // rotate to the end
-	while (count--)
+	while ((*n)--)
 	{
 		if (flag)
 		{
-			// rotate(st, RA);
+			rotate(st, RA);
 			ft_nodeadd_end(&arr->lis_tail, ft_nodepop(&arr->lis_head));
 		}
 		else
 		{
-			// rotate(st, RRA);
-			op = RRA; // rotate to the front
+			rotate(st, RRA);
 			arr->lis_tail = arr->lis_tail->prev;
 			ft_nodeadd_front(&arr->lis_head, arr->lis_tail->next);
 			arr->lis_tail->next = NULL;
 		}
-		rotate_stacks(st, op, (!st->stack_b), arr->mean);
 	}
 	ft_nodedel_front(&arr->lis_head);
 	push(st, PB);
+	if (st->stack_b->num <= arr->mean)
+		rotate(st, RB);
 	return (arr);
 }
 
@@ -100,20 +80,17 @@ static void	init_stacks(t_nums *arr, t_struct *st)
 	int	left;
 	int	right;
 
-	left = 0;
-	while (left != -1)
+	while (1)
 	{
 		n = &left;
 		left = get_closest_non_lis(arr->lis_head, 1);
 		right = get_closest_non_lis(arr->lis_tail, 0) + 1;
 		if (left > right)
 			n = &right;
-		if (left != -1)
-		{
-			arr = init_stackb(st, arr, n, (left < right));
-		}
+		if (left == -1)
+			break ;
+		arr = non_lis_to_stackb(st, arr, n, (left < right));
 	}
-	st->op_list = NULL;
 }
 
 int	main(int argc, char **argv)
@@ -130,9 +107,9 @@ int	main(int argc, char **argv)
 			arr.num[argc - 1] = validate_argv(&st, argv[argc]);
 			arr.mean += arr.num[argc - 1];
 		}
+		arr.mean /= arr.size;
 		get_lis(&arr);
 		init_stacks(&arr, &st);
-		// ft_printf("mean: %d\n", mean);
 		ft_nodeprint(&arr.lis_head);
 		print_list(&st);
 	}
