1499ba36398097f1b9112515cdb6a286d9e4871a
Leo MPB <vanleotran@gmail.com>
2022-06-03 23:04:16 +0300

    moved lis fn to lis_tools.c [skip ci]

--- push_swap/src/push_swap/lis_tools.c
+++ push_swap/src/push_swap/lis_tools.c
@@ -6,7 +6,7 @@
 /*   By: leo <leo@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/20 13:31:49 by leo               #+#    #+#             */
-/*   Updated: 2022/05/31 22:05:09 by leo              ###   ########.fr       */
+/*   Updated: 2022/06/03 23:03:56 by leo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,4 +58,71 @@ void	get_lis(t_nums *arr)
 	while (--j)
 		max = ft_max(max, arr->lis[j]);
 	init_lis_stack(arr, max, arr->size);
+}
+
+void	init_stacks(t_nums *arr, t_struct *st)
+{
+	int	*n;
+	int	left;
+	int	right;
+
+	while (1)
+	{
+		n = &left;
+		left = get_closest_non_lis(arr->lis_head, 1);
+		right = get_closest_non_lis(arr->lis_tail, 0) + 1;
+		if (left > right)
+			n = &right;
+		if (left == -1)
+			break ;
+		arr = non_lis_to_stackb(st, arr, n, (left < right));
+	}
+}
+
+int	get_closest_non_lis(t_node *lis, int flag)
+{
+	t_node	*current;
+	t_node	*temp;
+	int		res;
+
+	current = lis;
+	res = 0;
+	while (current)
+	{
+		temp = current;
+		if (current->num == 0)
+			break ;
+		else if (flag)
+			current = current->next;
+		else
+			current = current->prev;
+		res++;
+	}
+	if (!current && temp->num != 0)
+		res = -1;
+	return (res);
+}
+
+t_nums	*non_lis_to_stackb(t_struct *st, t_nums *arr, int *n, int flag)
+{
+	while ((*n)--)
+	{
+		if (flag)
+		{
+			rotate(st, RA);
+			ft_nodeadd_end(&arr->lis_tail, ft_nodepop(&arr->lis_head));
+		}
+		else
+		{
+			rotate(st, RRA);
+			arr->lis_tail = arr->lis_tail->prev;
+			ft_nodeadd_front(&arr->lis_head, arr->lis_tail->next);
+			arr->lis_tail->next = NULL;
+		}
+	}
+	ft_nodedel_front(&arr->lis_head);
+	push(st, PB);
+	if (st->stack_b->num <= arr->mean)
+		rotate(st, RB);
+	return (arr);
 }
--- push_swap/src/push_swap/push_swap.c
+++ push_swap/src/push_swap/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: leo <leo@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/11 23:38:59 by leo               #+#    #+#             */
-/*   Updated: 2022/06/03 22:56:58 by leo              ###   ########.fr       */
+/*   Updated: 2022/06/03 23:01:10 by leo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,73 +24,6 @@ static void	init_intarrays(t_nums *arr, int size)
 	arr->mean = 0;
 	while (size--)
 		arr->lis[size] = 1;
-}
-
-static int	get_closest_non_lis(t_node *lis, int flag)
-{
-	t_node	*current;
-	t_node	*temp;
-	int		res;
-
-	current = lis;
-	res = 0;
-	while (current)
-	{
-		temp = current;
-		if (current->num == 0)
-			break ;
-		else if (flag)
-			current = current->next;
-		else
-			current = current->prev;
-		res++;
-	}
-	if (!current && temp->num != 0)
-		res = -1;
-	return (res);
-}
-
-static t_nums	*non_lis_to_stackb(t_struct *st, t_nums *arr, int *n, int flag)
-{
-	while ((*n)--)
-	{
-		if (flag)
-		{
-			rotate(st, RA);
-			ft_nodeadd_end(&arr->lis_tail, ft_nodepop(&arr->lis_head));
-		}
-		else
-		{
-			rotate(st, RRA);
-			arr->lis_tail = arr->lis_tail->prev;
-			ft_nodeadd_front(&arr->lis_head, arr->lis_tail->next);
-			arr->lis_tail->next = NULL;
-		}
-	}
-	ft_nodedel_front(&arr->lis_head);
-	push(st, PB);
-	if (st->stack_b->num <= arr->mean)
-		rotate(st, RB);
-	return (arr);
-}
-
-static void	init_stacks(t_nums *arr, t_struct *st)
-{
-	int	*n;
-	int	left;
-	int	right;
-
-	while (1)
-	{
-		n = &left;
-		left = get_closest_non_lis(arr->lis_head, 1);
-		right = get_closest_non_lis(arr->lis_tail, 0) + 1;
-		if (left > right)
-			n = &right;
-		if (left == -1)
-			break ;
-		arr = non_lis_to_stackb(st, arr, n, (left < right));
-	}
 }
 
 int	main(int argc, char **argv)
