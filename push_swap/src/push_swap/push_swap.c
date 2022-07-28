b98c51a350baf894662400d63b4d9107bae3ca47
Leo MPB <vanleotran@gmail.com>
2022-07-28 08:15:23 +0300

    optimized sorting [skip ci]

--- push_swap/src/push_swap/push_swap.c
+++ push_swap/src/push_swap/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: leo <leo@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/11 23:38:59 by leo               #+#    #+#             */
-/*   Updated: 2022/07/28 07:50:53 by leo              ###   ########.fr       */
+/*   Updated: 2022/07/28 08:14:33 by leo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,24 +47,52 @@ static int	check_push_conditions(t_struct *st)
 	return (res);
 }
 
+static t_op	find_optimal_operation(t_struct *st, int list_size)
+{
+	t_node	*current;
+	t_node	*temp;
+	t_op	op;
+	int		count;
+
+	current = st->stack_a;
+	op = RA;
+	count = 0;
+	while (current)
+	{
+		temp = current;
+		if (current->num == st->max)
+			break ;
+		current = current->next;
+		count++;
+	}
+	if (count >= list_size / 2)
+		op = RRA;
+	return (op);
+}
+
 static void	sort_list(t_struct *st, t_nums *arr)
 {
-	ft_printf("Sort: \n");
+	t_op	op;
+
 	arr->mean = 0;
+	ft_printf("Sort: \n");
 	while (1)
 	{
-		if (check_if_sorted(st))
-			break ;
 		if (st->stack_b && check_push_conditions(st))
 		{
 			push(st, PA);
 			continue ;
 		}
-		if (st->stack_b && st->stack_b->num > st->stack_a->num)
+		if (!st->stack_b)
+			break ;
+		if (st->stack_b->num > st->stack_a->num)
 			rotate(st, RA);
 		else
 			rotate(st, RRA);
 	}
+	op = find_optimal_operation(st, arr->size);
+	while (!check_if_sorted(st))
+		rotate(st, op);
 }
 
 int	main(int argc, char **argv)
