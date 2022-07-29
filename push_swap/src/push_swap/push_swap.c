10b523002b775fc844540c64e109b7389b5b73b1
Leo MPB <vanleotran@gmail.com>
2022-07-30 02:26:45 +0300

    added print info for easier track of optimisation results [skip ci]

--- push_swap/src/push_swap/push_swap.c
+++ push_swap/src/push_swap/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: leo <leo@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/11 23:38:59 by leo               #+#    #+#             */
-/*   Updated: 2022/07/30 01:53:31 by leo              ###   ########.fr       */
+/*   Updated: 2022/07/30 02:21:47 by leo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,7 +59,9 @@ int	main(int argc, char **argv)
 			temp++;
 		}
 		get_lis_nums(&st, &arr);
-		// sort_list(&st, &arr);
+		sort_list(&st, &arr);
+		print_intarr(arr.lis , arr.size);
+		print_intarr(arr.num, arr.size);
 		ft_printf("mean: %d min: %d max: %d\n", arr.mean, st.min, st.max);
 		print_list(&st);
 	}
