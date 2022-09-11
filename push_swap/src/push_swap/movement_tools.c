dc1b81f184b69e3d6ded45daa98ad556a9801af8
Leo MPB <vanleotran@gmail.com>
2022-09-11 14:05:33 +0300

    back to fixing v1 [skip ci]

--- push_swap/src/push_swap/movement_tools.c
+++ push_swap/src/push_swap/movement_tools.c
@@ -6,7 +6,7 @@
 /*   By: leo <leo@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/20 15:05:34 by leo               #+#    #+#             */
-/*   Updated: 2022/09/11 01:19:34 by leo              ###   ########.fr       */
+/*   Updated: 2022/09/11 14:02:55 by leo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,48 +48,6 @@ static int	check_push(int a_prev, int a_next, int b, int a_max, int a_min)
 		res = 1;
 	return (res);
 }
-
-// static int	check_from_left(t_struct st, int *a, int *b)
-// {
-// 	t_node	*temp;
-// 	int		count;
-// 	int		temp_a_count;
-// 	int		min;
-// 	int		a_prev;
-
-// 	min = 0;
-// 	temp_a_count = 0;
-// 	a_prev = st.tail_a->num;
-// 	while (st.stack_a)
-// 	{
-// 		count = 0 + temp_a_count;
-// 		temp = st.stack_b;
-// 		// ft_printf("{YEL}stack_a loop current a->num: %d{EOC}\n", st.stack_a->num);
-// 		while (temp)
-// 		{
-// 			// ft_printf("stack_b loop current b->num: %d\nstackmin: %d stackmax: %d min: %d count: %d, ", temp->num, st.min, st.max, min, count);
-// 			// ft_printf("a_prev: %d a->next: %d ", a_prev, st.stack_a->num);
-// 			if (check_push(a_prev, st.stack_a->num, temp->num, st.max, st.min) && (min > count || min == 0))///not working properly
-// 			{
-// 				min = count;
-// 				*a = st.stack_a->num;
-// 				*b = temp->num;
-// 				// ft_printf("checkpush: {GRN}true\n[Steps a: %d b: %d min_count: %d]{EOC}\n", *a, *b, count);
-// 				break ;
-// 			}
-// 			// else
-// 				// ft_printf("checkpush: {RED}false{EOC}\n");
-// 			count++;
-// 			// count -= temp_a_count;
-// 			temp = temp->next;
-// 		}
-// 		a_prev = st.stack_a->num;
-// 		st.stack_a = st.stack_a->next;
-// 		temp_a_count++;
-// 	}
-// 	// ft_printf("target a: %d b: %d\n", *a, *b);
-// 	return (min);
-// }
 
 static int	check_from_left(t_struct st, int *a, int *b)
 {
